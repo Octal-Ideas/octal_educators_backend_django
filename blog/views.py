8ed8011eb5c1e93210c2a59a59eafe084df28eaf
principal kelvo <principalkelvo@gmail.com>
2023-02-18 23:39:12 +0300

    creates a viewset for category and blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.mixins import RetrieveModelMixin, ListModelMixin
+from rest_framework.viewsets import GenericViewSet
+from .serializers import BlogSerializer, CategorySerializer
+from .models import Blog, Category
 
 # Create your views here.
+
+
+class BlogViewSet(
+    GenericViewSet, ListModelMixin, RetrieveModelMixin
+):
+
+    serializer_class = BlogSerializer
+
+    def get_queryset(self):
+        queryset = Blog.objects.select_related('category').all()
+        category_id = self.request.query_params.get('category_id')
+
+        if category_id is not None:
+            queryset = queryset.filter(
+                category_id=category_id).select_related('category')
+
+        return queryset
+
+
+class CategoryViewSet(GenericViewSet, ListModelMixin,):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
