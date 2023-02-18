359d4707c1debbfc5ac524c26fe32a730ebe9b6f
principal kelvo <principalkelvo@gmail.com>
2023-02-18 23:18:13 +0300

    creates category and blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    thumbnail = CloudinaryField('image')
+    description = models.TextField()
+    slug = models.SlugField(default='-')
+    last_update = models.DateField(auto_now=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name='blogs')
+
+    def __str__(self) -> str:
+        return self.title
