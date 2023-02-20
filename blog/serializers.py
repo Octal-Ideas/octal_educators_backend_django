140a3e15d3be768fb99e2e9d2dd809e9146fd2f6
principal kelvo <kelvinnjoroowino@gmail.com>
2023-02-20 23:33:59 +0300

    createsserializers for tags,comments and viewcounts

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Blog, Category
+from .models import Blog, Category, Tag, Comment,ViewCount
 
 
 
@@ -8,12 +8,33 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ('id', 'title')
 
+# Serializes the Tag model
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
 
+# Serializes the Comment model
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'content',  'author')
+        read_only_fields =('date_posted','modified_at')
         
 class BlogSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
+    tags = TagSerializer(many=True)
+    comments = CommentSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Blog
         fields = ('id', 'title', 'thumbnail', 'description',
-                  'slug', 'category')
-        read_only_fields = ('created_by', 'created_at', 'modified_at')
+                  'slug', 'category', 'tags', 'comments')
+        read_only_fields = ('created_by', 'created_at', 'modified_at','published_date', 'duration')
+        
+class ViewCountSerializer(serializers.ModelSerializer):
+    blog_post = serializers.SlugRelatedField(slug_field='slug', queryset=Blog.objects.all())
+    
+    class Meta:
+        model = ViewCount
+        fields = ('id', 'blog_post', 'count')
