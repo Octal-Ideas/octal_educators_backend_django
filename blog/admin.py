8c7ea5b8ece9ed26cd94c9c2cae1a8731a7cc675
principal kelvo <kelvinnjoroowino@gmail.com>
2023-02-20 23:31:46 +0300

    view all models on admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Blog,Category
+from .models import Blog,Category, Tag, Comment,ViewCount
 
 admin.site.register(Blog)
 admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(ViewCount)
