6a526c6e381baa4fb4b9fda0ead8de30b610d51a
principal kelvo <kelvinnjoroowino@gmail.com>
2023-02-20 23:34:44 +0300

    urls for comments and view counts

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,7 @@ router = DefaultRouter()
 
 router.register('blogs', views.BlogViewSet, basename='blogs'),
 router.register('categories', views.CategoryViewSet, basename='categories')
+router.register('comments', views.CommentViewSet, basename='comments')
+router.register('view-counts', views.ViewCountViewSet, basename='view-counts')
 
-urlpatterns =[path(' ',include(router.urls))]
+urlpatterns =[path('', include(router.urls))]
