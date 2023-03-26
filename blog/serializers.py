9a8d8bf914d298d5b846e923ff159c5e3326414d
principal kelvo <principalkelvo@gmail.com>
2023-03-26 09:44:09 +0300

    validates title and description

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,32 +1,20 @@
 from rest_framework import serializers
-from .models import Blog, Category, Tag, Comment,ViewCount
+from .models import Blog, Category, Comment,ViewCount
+from taggit.serializers import (TagListSerializerField, TaggitSerializer)
 
 
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('id', 'title')
-
-# Serializes the Tag model
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = ('id', 'name')
 
 # Serializes the Comment model
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ('id', 'content',  'author')
+        fields = ('id', 'content',  'author', 'post')
         read_only_fields =('date_posted','modified_at', 'pub_date')
         
-class BlogSerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
-    tags = TagSerializer(many=True)
+class BlogSerializer(TaggitSerializer, serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
     time_since_published = serializers.SerializerMethodField()
-    
+    tags = TagListSerializerField()
     class Meta:
         model = Blog
         fields = ('id', 'title', 'thumbnail', 'description',
@@ -35,6 +23,24 @@ class BlogSerializer(serializers.ModelSerializer):
         
     def get_time_since_published(self, obj):
         return obj.whenpublished()
+    
+    def validate_title(self, value):
+        if len(value) > 100:
+            return serializers.ValidationError("Max title length is 100 characters")
+        return value
+    
+    def validate_description(self, value):
+        if len(value) > 200:
+            return serializers.ValidationError(
+                "Max description length is 200 characters"
+            )
+        return value
+    
+class CategorySerializer(serializers.ModelSerializer):
+    blogs = BlogSerializer(many = True)
+    class Meta:
+        model = Category
+        fields = ('id', 'title','blogs')
         
 class ViewCountSerializer(serializers.ModelSerializer):
     blog_post = serializers.SlugRelatedField(slug_field='slug', queryset=Blog.objects.all())
