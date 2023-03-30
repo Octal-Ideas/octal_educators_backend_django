eb91328a3d04d2d2aee00f7c75922c9ff3c5bc2e
principal kelvo <principalkelvo@gmail.com>
2023-03-31 00:27:21 +0300

    creates a custom user and adds roles the roles default to blogger user uses email

--- account/models.py
+++ account/models.py
@@ -1,8 +1,24 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from phonenumber_field.modelfields import PhoneNumberField
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    def create_user(self, email, username=None, password=None, phone_number=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email,username=None, password=password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
     ROLES = (
         ('admin', 'Admin'),
         ('teacher', 'Teacher'),
@@ -12,34 +28,27 @@ class User(AbstractUser):
     )
     role = models.CharField(max_length=10, choices=ROLES, default='blogger')
     email = models.EmailField(unique=True)
-    phone_number = PhoneNumberField( unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, unique=True, blank=True, null=True)
+    phone_number = PhoneNumberField(blank=True, null=True)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='custom_user_set',
-        blank=True,
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='custom_user_set',
-        blank=True,
-    )
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
 
-    def save(self, *args, **kwargs):
-        if not self.username:
-            base_username = f"{self.first_name[0]}{self.last_name}"
-            if User.objects.filter(username=base_username).exists():
-                base_username = f"{self.first_name[:2]}{self.last_name}"
-                i = 1
-                while User.objects.filter(username=f"{base_username}{i}").exists():
-                    i += 1
-                self.username = f"{base_username}{i}"
-            else:
-                self.username = base_username
-        super().save(*args, **kwargs)
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
 
+    def get_short_name(self):
+        return self.first_name
+
+    def get_role(self):
+        return self.role
