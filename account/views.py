cd867e4b5b9ff2d35cc22c1da3a40597a328b939
principal kelvo <principalkelvo@gmail.com>
2023-04-05 22:32:13 +0300

    a simple view with permissions to view all users you can view all users if you are an admin not yet tested

--- account/views.py
+++ account/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from rest_framework import status
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import User
+from .serializers import UserSerializer
+
+
+
+
 # Create your views here.
+class UserList(APIView):
+    permission_classes = [IsAdminUser | AllowAny]
+
+    def get(self, request):
+        if not request.user.is_staff:
+            return Response({'error': 'Only admins can view users.'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # def post(self, request):
+    #     # print(request.data)
+    #     serializer = UserSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    
