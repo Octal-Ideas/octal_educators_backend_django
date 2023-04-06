67c745f746c3232929e81cd34e6f5de3b95e4f0d
principal kelvo <principalkelvo@gmail.com>
2023-04-06 13:43:14 +0300

    use postgress as primary db

--- octaleducatorsbackend/settings.py
+++ octaleducatorsbackend/settings.py
@@ -133,24 +133,24 @@ WSGI_APPLICATION = 'octaleducatorsbackend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# if os.environ.get('ENV') == 'production':
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'containers-us-west-121.railway.app',
+        'PORT': '5764',
     }
 }
-
-if os.environ.get('ENV') == 'production':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'railway',
-            'USER': 'postgres',
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': 'containers-us-west-121.railway.app',
-            'PORT': '5764',
-        }
-    }
 
 # if 'test' in sys.argv:
 #     DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
