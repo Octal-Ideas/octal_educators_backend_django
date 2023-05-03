cd3dfda29917e59e89c3e0bbbc12aa2b1b0ddc91
principal Kelvo <kelvinnjoroowino@gmail.com>
2023-05-03 17:54:51 +0000

    allowed host to all

--- octaleducatorsbackend/settings.py
+++ octaleducatorsbackend/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', "127.0.0.1"]
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
