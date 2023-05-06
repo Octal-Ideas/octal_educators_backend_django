786f676c8bc2083ce3b094279f35c3067c1f58c0
principal kelvo <kelvinnjoroowino@gmail.com>
2023-05-06 11:27:41 +0300

    adds ckeditor to serializer

--- account/serializers.py
+++ account/serializers.py
@@ -5,7 +5,7 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['id', 'role', 'email', 'username', 'phone_number', 'first_name','password', 'last_name']
+        fields = ["id", "role", "email", "username", "phone_number", "first_name","password", "last_name"]
         extra_kwargs = {'password': {'write_only': True}}
 
 
