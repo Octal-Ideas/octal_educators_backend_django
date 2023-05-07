fc51209681c518521af656388ed021bc693cb5d3
principal kelvo <kelvinnjoroowino@gmail.com>
2023-05-07 10:38:44 +0300

    creates a class based view,  all crud operation the courses, lectures, teachers and department

--- course/views.py
+++ course/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from rest_framework.throttling import UserRateThrottle
+
+from .models import Course, Department, Teacher, Lecture
+from .serializers import CourseSerializer, DepartmentSerializer, TeacherSerializer, LectureSerializer
+from .pagination import CourseLimitOffsetPagination
+from .throttles import CourseRateThrottle
+class DepartmentList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+class DepartmentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+class TeacherList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class LectureList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Lecture.objects.all()
+    serializer_class = LectureSerializer
+
+class LectureDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Lecture.objects.all()
+    serializer_class = LectureSerializer
+
+class CourseList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer 
+    pagination_class = CourseLimitOffsetPagination
+    throttle_classes = [UserRateThrottle, CourseRateThrottle]
+
+class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
