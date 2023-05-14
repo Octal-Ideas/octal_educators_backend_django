24953f9ed635de0773caa75e2c0342d0709761fb
principal kelvo <kelvinnjoroowino@gmail.com>
2023-05-14 22:05:22 +0300

    serializer for the fields

---
+++ lead/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Lead
+
+class LeadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lead
+        fields = ["id", "email", "phone_number", "first_name", "last_name"]
+        read_only_fields = ('created_by', 'created_at', 'modified_at','status','priority')
