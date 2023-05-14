7460b1a304115d9c1bc3524834ee8942a37b8259
principal kelvo <kelvinnjoroowino@gmail.com>
2023-05-14 22:01:20 +0300

    creates models for a lead the status is new, and it will be update by the admin, also the priority can only be updated by admin

--- lead/models.py
+++ lead/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.conf import settings
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
+CHOICES_PRIORITY = (("low", 'Low'), ("medium", 'Medium'), ("high", 'High'))
+CHOICES_STATUS = (("new", 'New'), ("contacted", 'Contacted'),
+                  ("inprogress", 'In progress'), ("lost", 'Lost'), ("won", 'Won'))
+
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.EmailField(unique=True)
+    phone_number = PhoneNumberField(unique=True)
+    status = models.CharField(
+        max_length=20, choices=CHOICES_STATUS, default='new')
+    priority = models.CharField(
+        max_length=20, choices=CHOICES_PRIORITY, default='low')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
