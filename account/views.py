318c1afb17cf775e0b81189a7358fc7294357046
principal kelvo <kelvinnjoroowino@gmail.com>
2023-05-18 14:40:05 +0300

    adds permission to only admins can change lead to student shows password before changing lead to student

--- account/views.py
+++ account/views.py
@@ -9,14 +9,14 @@ from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.throttling import UserRateThrottle
-from rest_framework.decorators import action, api_view
+from rest_framework.decorators import permission_classes, api_view
+
 
 from .throttles import AccountsRateThrottle
 from .models import User
 from .serializers import UserSerializer
 
 from lead.models import Lead
-from lead.serializers import LeadSerializer
 
 
 # Creating a view for the UserList
@@ -67,6 +67,7 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 
 @api_view(['POST'])
+@permission_classes([IsAdminUser])
 def convert_lead_to_student_view(request):
     # Get the ID of the Lead object from the request data
     lead_id = request.data.get('lead_id')
@@ -76,8 +77,9 @@ def convert_lead_to_student_view(request):
         new_student = convert_lead_to_student(lead_id)
 
         # Serialize the new Student object and return it as a response
-        serializer = LeadSerializer(new_student)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # serializer = LeadSerializer(new_student)
+        message = 'Student successfully created'
+        return Response({'message': message}, status=status.HTTP_201_CREATED)
     except Lead.DoesNotExist:
         # Return a 404 error if the Lead object with the given ID does not exist
         return Response(status=status.HTTP_404_NOT_FOUND)
@@ -86,7 +88,6 @@ def convert_lead_to_student_view(request):
 def convert_lead_to_student(lead_id):
     # Get the Lead object with the given ID
     lead = Lead.objects.get(id=lead_id)
-    password = make_password(User.objects.make_random_password())
     # Create a new Student object with the same data as the Lead object
     student = User.objects.create(
         first_name=lead.first_name,
@@ -94,8 +95,13 @@ def convert_lead_to_student(lead_id):
         email=lead.email,
         phone_number=lead.phone_number,
         role='student',
-        password=password,
     )
+    # Generate a random password
+    password = User.objects.make_random_password()
+
+    # Set the password for the new student
+    student.set_password(password)
+    student.save()
 
     # Delete the Lead object
     lead.delete()
@@ -104,6 +110,6 @@ def convert_lead_to_student(lead_id):
     # and instructions on how to reset their password upon logging in
     # todo send_password_email(student.email, password)
 
-    # print(student.email, password)
+    print(student.email, password)
     # Return the new Student object
     return student
