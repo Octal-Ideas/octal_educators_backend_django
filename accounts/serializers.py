62138b0a5ec9824e27c3212a537942849d044520
principal kelvo <kelvinnjoroowino@gmail.com>
2023-05-21 22:55:50 +0300

    acounts no updates

---
+++ accounts/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ["id", "role", "email", "username", "phone_number", "first_name","password", "last_name"]
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+
