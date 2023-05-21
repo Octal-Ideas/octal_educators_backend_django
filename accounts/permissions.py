fb8b5d570442322865ea701565c7438b79adfa1d
principal kelvo <kelvinnjoroowino@gmail.com>
2023-05-21 22:52:26 +0300

    creates custom permissions updates to check if user is logged in first

---
+++ accounts/permissions.py
@@ -0,0 +1,50 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+# Custom permission class to allow read operations to all users and write operations only to teacher users
+class IsTeacherOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_authenticated and request.user.role == 'teacher'
+
+
+# Custom permission class to allow read operations to all users and write operations only to parent users
+class IsParentOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_authenticated and request.user.role == 'parent'
+
+
+# Custom permission class to allow read operations to all users and write operations only to student users
+class IsStudentOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_authenticated and request.user.role == 'student'
+
+
+# Custom permission class to allow read operations to all users and write operations only to blogger users
+class IsBloggerOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_authenticated and request.user.role == 'blogger'
+
+
+# Custom permission class to allow only authenticated student users
+class IsStudent(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'student'
+
+
+# Custom permission class to allow only authenticated teacher users
+class IsTeacher(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'teacher'
+
+
+# Custom permission class to allow only authenticated parent users
+class IsParent(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'parent'
