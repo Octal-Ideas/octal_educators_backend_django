8737921971f2e9c3e8668792d658bb32781728fe
principal kelvo <kelvinnjoroowino@gmail.com>
2023-06-04 00:43:00 +0300

    Create an API that sends email notifications to subscribers when new blog posts are published on the school's website. Fixes #18

--- notification/models.py
+++ notification/models.py
@@ -1,34 +1,32 @@
-from django.db import models
-
 import uuid
 
 from django.db import models
 
 from account.models import User
-from post.models import Post
+from blog.models import Blog
 # Create your models here.
 
 
 class Notification(models.Model):
-    NEWFRIENDREQUEST = 'new_friendrequest'
-    ACCEPTEDFRIENDREQUEST = 'accepted_friendrequest'
-    REJECTEDFRIENDREQUEST = 'rejected_friendrequest'
-    POST_LIKE = 'post_like'
-    POST_COMMENT = 'post_comment'
+    NEWBLOG = 'new_blog'
+    SUBSCRIBED = 'subscribed_blog'
+    UNSUBSCRIBED = 'unsubscribed_blog'
+    BLOG_LIKE = 'blog_like'
+    BLOG_COMMENT = 'blog_comment'
 
     CHOICES_TYPE_OF_NOTIFICATION = (
-        (NEWFRIENDREQUEST, 'New friendrequest'),
-        (ACCEPTEDFRIENDREQUEST, 'Accepted friendrequest'),
-        (REJECTEDFRIENDREQUEST, 'Rejected friendrequest'),
-        (POST_LIKE, 'Post like'),
-        (POST_COMMENT, 'Post comment')
+        (NEWBLOG, 'New blog'),
+        (SUBSCRIBED, 'subscribed blog'),
+        (UNSUBSCRIBED, 'Rejected friendrequest'),
+        (BLOG_LIKE, 'Blog like'),
+        (BLOG_COMMENT, 'Blog comment')
     )
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     body = models.TextField()
     is_read = models.BooleanField(default=False)
     type_of_notification = models.CharField(max_length=50, choices=CHOICES_TYPE_OF_NOTIFICATION)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, blank=True, null=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, blank=True, null=True)
     created_by = models.ForeignKey(User, related_name='created_notifications', on_delete=models.CASCADE)
     created_for = models.ForeignKey(User, related_name='received_notifications', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
