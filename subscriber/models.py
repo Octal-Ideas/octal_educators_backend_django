21a4598854193da3c49a98234d668db5cc264aab
principalkelvo <kelvinnjoroowino@gmail.com>
2023-06-20 15:33:53 +0300

    subscription gets all users you have subscribed to

--- subscriber/models.py
+++ subscriber/models.py
@@ -1,18 +1,26 @@
 import uuid
 from django.db import models
 
-# Create your models here.
 from accounts.models import User
 # Create your models here.
 
 
 class Subscriber(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    subscribers = models.ManyToManyField(User, related_name='subscriptions')
-    created_at = models.DateTimeField(auto_now_add=True)
-    content_owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='subscribers')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='subscriber')
+    
+    #collects all users you have subscribed to
+    subscription = models.ManyToManyField(
+        User, related_name='subscriptions', blank=True)
 
     def __str__(self):
-        return f'{self.user.username}'
+        return self.user.username
+
+    #gets all subscribed users
+    def get_subscribed_users(self):
+        return self.subscription.all()
+
+    #gets all users who have subscribed to your account
+    def get_subscriber_users(self):
+        return User.objects.filter(subscriptions__user=self.user)
