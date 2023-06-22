0c0da5e28682ee46da8b8bc076ff194fc98e3b2b
principalkelvo <kelvinnjoroowino@gmail.com>
2023-06-22 12:43:07 +0300

    a working notification model checks notification if is read or not. relates to user created for and the one who created can list and mark as read only

--- notification/models.py
+++ notification/models.py
@@ -1,8 +1,28 @@
+from uuid import uuid4
 from django.db import models
-import uuid
+from django.conf import settings
+
 
 class Notification(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+    NOTIFICATION_TYPES = [
+        ('blog_like', 'Blog Like'),
+        ('blog_comment', 'Blog Comment'),
+        ('new_blog', 'New Blog'),
+        ('subscribe', 'Subscribe'),
+        ('unsubscribe', 'Unsubscribe'),
+    ]
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    is_read = models.BooleanField(default=False)
+    body = models.TextField()
+    type_of_notification = models.CharField(
+        max_length=20, choices=NOTIFICATION_TYPES)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    blog = models.ForeignKey(
+        'blog.Blog', on_delete=models.CASCADE, null=True, blank=True)
+    created_for = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='notifications', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.type_of_notification} - {self.created_by} to {self.created_for}"
