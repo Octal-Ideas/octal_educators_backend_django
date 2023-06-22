7ea670f1ebb8b86409bd25ec325ee141d47e843f
principalkelvo <kelvinnjoroowino@gmail.com>
2023-06-22 12:43:52 +0300

    crates notification on blog creation and liked blog

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 # todo add celery to make this send email work
 from task_manager.tasks import send_email_task
 
@@ -9,16 +8,17 @@ from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.permissions import BasePermission, SAFE_METHODS
-# from rest_framework.generics import RetrieveUpdateDestroyAPIView
-from rest_framework.throttling import UserRateThrottle 
+from rest_framework.throttling import UserRateThrottle
 
 from .serializers import BlogSerializer, CategorySerializer, CommentSerializer, ViewCountSerializer
 from .models import Blog, Category, Comment, ViewCount, Like
+
+from notification.utils import create_notification
+from accounts.permissions import ReadOnlyOrAuthenticated
+
 from .pagination import PostLimitOffsetPagination
 from .throttles import BlogRateThrottle
 
-from accounts.permissions import ReadOnlyOrAuthenticated
 
 # class ReadOnlyOrAuthenticated(BasePermission):
 #     # Custom permission class that allows read access to anyone, but only allows
@@ -30,20 +30,20 @@ from accounts.permissions import ReadOnlyOrAuthenticated
 
 # Viewset for the Blog model
 class BlogViewSet(
-    GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateModelMixin,UpdateModelMixin,
+    GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin,
 ):
     # Sets the serializer class and permission class to be used by this viewset
     serializer_class = BlogSerializer
     permission_classes = [ReadOnlyOrAuthenticated]
     pagination_class = PostLimitOffsetPagination
     throttle_classes = [UserRateThrottle, BlogRateThrottle]
-     
+
     def get_queryset(self):
         # Returns all Blog objects and filters based on the category_id query parameter if it exists
         queryset = Blog.objects.select_related('category').all()
         category_id = self.request.query_params.get('category_id')
         print("get all blogs by category")
-    
+
         if category_id is not None:
             queryset = queryset.filter(
                 category_id=category_id)
@@ -51,16 +51,15 @@ class BlogViewSet(
 
     def perform_create(self, serializer):
         # When a new Blog object is created, sets the created_by field to the current user
-        print("create owner of the blog")
-        if serializer.is_valid(raise_exception=True):         
+        if serializer.is_valid(raise_exception=True):
             serializer.validated_data['created_by'] = self.request.user
             serializer.save()
             print("created owner of the blog")
+            
             return Response(serializer.data, status=200)
         else:
             return Response({"errors": serializer.errors}, status=400)
-        
-    
+
 
 # Viewset for the Category model
 class CategoryViewSet(GenericViewSet, ListModelMixin):
@@ -70,22 +69,28 @@ class CategoryViewSet(GenericViewSet, ListModelMixin):
     permission_classes = [ReadOnlyOrAuthenticated]
 
 # Viewset for the Comment model
-class CommentViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateModelMixin,UpdateModelMixin,):
+
+
+class CommentViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin,):
     # Sets the queryset, serializer class, and permission class to be used by this viewset
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [ReadOnlyOrAuthenticated]
-    
+
     def post(self, request, slug, serializer, *args, **kwargs):
         # When a new Comment object is created, sets the created_by and post fields to the current user and the specified Blog object respectively
         post = get_object_or_404(Blog, slug=slug)
         if serializer.is_valid(raise_exception=True):
             serializer.save(created_by=request.user, post=post)
+            notification = create_notification(
+            request, 'blog_comment', post_id=post.id)
             return Response(serializer.data, status=200)
         else:
             return Response({"errors": serializer.errors}, status=400)
 
 # # Viewset to handle view count operations for blog posts
+
+
 class ViewCountViewSet(GenericViewSet):
     # Sets the queryset and serializer class to be used by this viewset
     queryset = ViewCount.objects.all()
@@ -98,14 +103,16 @@ class ViewCountViewSet(GenericViewSet):
         return obj
 
     # Action to increment the view count for the specified blog post
-    @action(detail=True, methods=['post'],name='increment_viewers')
+    @action(detail=True, methods=['post'], name='increment_viewers')
     def increment_viewers(self, request, *args, **kwargs):
         instance = self.get_object()
         instance.increment_viewers()
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
-    
- # Function-based view to handle creating likes for blog posts   
+
+ # Function-based view to handle creating likes for blog posts
+
+
 @api_view(['POST'])
 def post_like(request, pk):
     post = Blog.objects.get(pk=pk)
@@ -119,49 +126,14 @@ def post_like(request, pk):
         post.likes_count = post.likes_count + 1
         post.likes.add(like)
         post.save()
+        notification = create_notification(
+            request, 'post_like', post_id=post.id)
 
         return JsonResponse({'message': 'like created'})
     else:
         return JsonResponse({'message': 'post already liked'})
-    
-# subscribers
-@login_required
-def subscribe_to_blog(request, blog_id):
-    blog = get_object_or_404(Blog, id=blog_id)
-    blog.subscribers.add(request.user)
-    return JsonResponse({'message': 'subscribed'})
 
-@login_required
-def unsubscribe_from_blog(request, blog_id):
-    blog = get_object_or_404(Blog, id=blog_id)
-    blog.subscribers.remove(request.user)
-    return JsonResponse({'message': 'unsubscribed'})
-
-@login_required
-def is_user_subscribed(request, blog_id):
-    blog = Blog.objects.filter(id=blog_id, subscribers=request.user).exists()
-    return JsonResponse({'subscribed': blog})
-
-@login_required
-def list_subscribers(request, blog_id):
-    try:
-        blog = Blog.objects.get(id=blog_id)
-    except Blog.DoesNotExist:
-        return JsonResponse({'error': 'Blog not found'}, status=404)
-
-    if blog.owner == request.user or request.user.is_superuser:
-        # User is the owner of the blog or an admin
-        subscribers = blog.subscribers.all()
-        subscriber_list = [subscriber.username for subscriber in subscribers]
-
-        return JsonResponse({'subscribers': subscriber_list})
-    else:
-        # User is not the owner or an admin, only return the total number of subscribers
-        subscriber_count = blog.subscribers.count()
-
-        return JsonResponse({'subscriber_count': subscriber_count})
-    
-#TODO class based likes
+# TODO class based likes
 # class PostLikeView(APIView):
 #     def post(self, request, pk):
 #         post = Blog.objects.get(pk=pk)
@@ -179,6 +151,8 @@ def list_subscribers(request, blog_id):
 #             return JsonResponse({'message': 'post already liked'})
 
 # !not working, waiting for celery
+
+
 def send_email(request):
     # Code to gather the email subject, message, from_email, and recipient_list
     subject = 'Hello'
