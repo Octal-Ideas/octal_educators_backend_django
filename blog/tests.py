76689783d85080c0e9d9f0d9ad4a42314034a276
principalkelvo <kelvinnjoroowino@gmail.com>
2023-06-23 10:20:40 +0300

    adds notification to test

--- blog/tests.py
+++ blog/tests.py
@@ -4,6 +4,8 @@ from rest_framework.test import APIRequestFactory, force_authenticate
 from .models import Blog, ViewCount, Comment, Like, Category
 from .views import BlogViewSet, CategoryViewSet, CommentViewSet, ViewCountViewSet, post_like
 from accounts.models import User
+from notification.models import Notification
+
 
 class BlogViewSetTestCase(TestCase):
     def setUp(self):
@@ -28,9 +30,10 @@ class BlogViewSetTestCase(TestCase):
         request = self.factory.post(url, data)
         force_authenticate(request, user=self.user)
         response = self.viewset(request)
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(Blog.objects.count(), 2)
         self.assertEqual(Blog.objects.last().title, 'New Blog')
+
 
 class CategoryViewSetTestCase(TestCase):
     def setUp(self):
@@ -43,6 +46,7 @@ class CategoryViewSetTestCase(TestCase):
         response = self.viewset(request)
         self.assertEqual(response.status_code, 200)
 
+
 class CommentViewSetTestCase(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
@@ -51,7 +55,8 @@ class CommentViewSetTestCase(TestCase):
         self.category = Category.objects.create(title='Test Category')
         self.blog = Blog.objects.create(
             title='Test Blog', content='Test Content', category=self.category, created_by_id=self.user.id, slug='test-blog')
-        self.viewset = CommentViewSet.as_view({'get': 'list', 'post': 'create'})
+        self.viewset = CommentViewSet.as_view(
+            {'get': 'list', 'post': 'create'})
 
     def test_comment_list(self):
         url = reverse('comments-list')
@@ -69,6 +74,7 @@ class CommentViewSetTestCase(TestCase):
         self.assertEqual(Comment.objects.count(), 1)
         self.assertEqual(Comment.objects.last().content, 'New Comment')
 
+
 class ViewCountViewSetTestCase(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
@@ -76,6 +82,7 @@ class ViewCountViewSetTestCase(TestCase):
         self.blog = Blog.objects.create(
             title='Test Blog', content='Test Content', category=self.category)
         self.viewset = ViewCountViewSet.as_view({'post': 'increment_viewers'})
+
 
 class PostLikeViewTestCase(TestCase):
     def setUp(self):
@@ -88,12 +95,20 @@ class PostLikeViewTestCase(TestCase):
         self.url = reverse('post_like', kwargs={'pk': self.blog.id})
 
     def test_post_like(self):
-        request = self.factory.post(self.url)
+        url = reverse('post_like', kwargs={'pk': self.blog.pk})
+        request = self.factory.get(url)
         force_authenticate(request, user=self.user)
-        response = post_like(request, pk=self.blog.id)
+        response = self.client.post(url)
+
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(Like.objects.count(), 1)
+        self.assertEqual(response.data, {'message': 'like created'})
         self.assertEqual(self.blog.likes_count, 1)
+        self.assertEqual(self.blog.likes.first().created_by, self.user)
+        self.assertEqual(Notification.objects.count(), 1)
+        notification = Notification.objects.first()
+        self.assertEqual(notification.notification_type, 'post_like')
+        self.assertEqual(notification.post_id, self.blog.id)
+        self.assertEqual(notification.recipient, self.blog.created_by)
 
     def test_post_like_already_liked(self):
         like = Like.objects.create(created_by=self.user)
